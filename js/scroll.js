80451b9cc13c19dbf89675761ecdf1a815407ed2
Kei5665 <64511596+Kei5665@users.noreply.github.com>
2020-07-11 07:28:08 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,8 @@
 $(function(){
   // #で始まるリンクをクリックしたら実行されます
   $('.b2').click(function() {
-    // スクロールの速度
-    var speed = 400; // ミリ秒で記述
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    return false;
+    $("html,body").animate({scrollTop:$('#contact').offset().top});
   });
 
-  $(window).scroll(function (){
-		$('.fadein').each(function(){
-			var elemPos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if (scroll > elemPos - windowHeight + 300){
-				$(this).addClass('scrollin');
-			}
-		});
-	});
   
 });
