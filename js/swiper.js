2b0744f0c52fb3f216342b82dfde34c3bebfcd76
Kei5665 <64511596+Kei5665@users.noreply.github.com>
2020-07-10 18:45:43 +0900

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,20 @@
 
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
+  slidesPerView: 1,
+  breakpoints: {
+    // 768px以上の場合
+    768: {
+      slidesPerView: 2,
+    },
+    // 980px以上の場合
+    980: {
+      slidesPerView: 3,
+    },
+    // 1200px以上の場合
+    1200: {
+      slidesPerView: 4,
+    }
+  },
   loop: true,
   autoplay: {
     delay: 2500,
